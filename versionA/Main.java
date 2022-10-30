63ef24cd9cb4eccae395b617072a3ce6ee7f06d6
IT21386336 <105857381+Dulajh@users.noreply.github.com>
2022-10-30 09:19:53 -0700

    Add files via upload
    Signed-off-by: IT21386336 <105857381+Dulajh@users.noreply.github.com>

---
+++ versionA/Main.java
@@ -0,0 +1,14 @@
+package versionA;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Lottory lb = new Lottory();
+        lb.checkLottory();
+        
+        
+        }
+	}
+
+
